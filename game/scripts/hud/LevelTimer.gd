2811cc85e27ff0329f85b7fcd58452852fcb49fd
Petra Baranski <info@progsource.de>
2020-01-31 23:07:35 +0100

    LevelTimer outputs now with 00:00 format

--- game/scripts/hud/LevelTimer.gd
+++ game/scripts/hud/LevelTimer.gd
@@ -13,12 +13,18 @@ func _process(delta):
 
 
 func _on_StartButton_start_button_pressed():
+	$Label.text = get_time_as_time_string(leftTime)
 	$Timer.start()
 
 
 func _on_Timer_timeout():
 	leftTime -= 1
-	$Label.text = String(leftTime)
+	$Label.text = get_time_as_time_string(leftTime)
 	if leftTime == 0:
 		$Timer.stop()
 		emit_signal("level_ended")
+
+func get_time_as_time_string(time : int) -> String:
+	var minutes = floor(time / 60);
+	var seconds = time % 60;
+	return "%02d:%02d" % [minutes, seconds]
