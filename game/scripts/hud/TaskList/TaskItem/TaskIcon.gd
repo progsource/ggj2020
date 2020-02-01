d043a9d431a0dfbb03fd917fc5a0fc37febd18d5
Agata Raap <agata.raap@posteo.de>
2020-02-01 02:03:08 +0100

    feat(task card): load icon

---
+++ game/scripts/hud/TaskList/TaskItem/TaskIcon.gd
@@ -0,0 +1,13 @@
+extends Node
+
+export var type: String = "Mobile"
+
+var mobile = preload("res://packed/items/Mobile.tscn")
+
+func _ready():
+	var icon
+	match type:
+		"Mobile":
+			icon = mobile.instance()
+
+	add_child(icon)
---
+++ game/scripts/items/Mobile.gd
@@ -0,0 +1,8 @@
+extends Object
+
+export var name = "Mobile"
+export var type = "Mobile"
+export var materials = [
+	"a",
+	"b"
+]
