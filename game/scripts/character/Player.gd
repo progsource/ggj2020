eeb8aba227cd4509cf12fed314c1366455c299ec
Petra Baranski <info@progsource.de>
2020-02-01 02:49:02 +0100

    Code Cleanup

--- game/scripts/character/Player.gd
+++ game/scripts/character/Player.gd
@@ -8,6 +8,7 @@ func _ready():
 		# warning-ignore:return_value_discarded
 		start_button.connect("start_button_pressed", self, "_on_start_button_pressed")
 
+
 func _physics_process(delta):
 	if !can_move:
 		return
@@ -20,20 +21,16 @@ func _physics_process(delta):
 	
 	var movement = speed * direction * delta
 	
-	#if attack_playing:
-	#	movement *= 0.3
-	
 	# warning-ignore:return_value_discarded
 	move_and_collide(movement)
 	
 	#if !attack_playing:
 	animate_player(direction)
-		
-	#if direction != Vector2.ZERO:
-	#	$RayCast2D.cast_to = direction.normalized() * 8
+
 
 func _on_start_button_pressed():
 	can_move = true
+
 
 func animate_player(direction: Vector2):
 	if direction != Vector2.ZERO:
