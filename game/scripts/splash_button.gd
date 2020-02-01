c4b59148a3efc01bf10d24ef2bb3a680e92e66d6
Agata Raap <agata.raap@posteo.de>
2020-02-01 16:38:35 +0100

    feat(button): connect to controller button index 0

--- game/scripts/splash_button.gd
+++ game/scripts/splash_button.gd
@@ -1,19 +1,16 @@
 extends Button
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
+func _input(event):
+	# button_index 0 = XBox - A
+	# button_index 1 = XBox - B
+	# button_index 2 = XBox - Y
+	# button_index 3 = XBox - X
+	if event is InputEventJoypadButton and event.button_index == 0:
+		_pressed()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
 func _pressed():
 	# warning-ignore:return_value_discarded
 	get_tree().change_scene("res://scenes/Level.tscn")
