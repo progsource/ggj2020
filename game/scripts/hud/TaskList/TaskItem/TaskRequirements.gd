78f86b87ae0b5803465ebac8e19efab77f119948
Agata Raap <agata.raap@posteo.de>
2020-02-02 13:35:01 +0100

    feat(checkmark): task list

--- game/scripts/hud/TaskList/TaskItem/TaskRequirements.gd
+++ game/scripts/hud/TaskList/TaskItem/TaskRequirements.gd
@@ -8,7 +8,15 @@ var pos: Array = [
 	Vector2(28, 7),
 ]
 
+var checkmarks: Array = [
+	Node2D.new(),
+	Node2D.new(),
+	Node2D.new(),
+	Node2D.new(),
+]
+
 onready var tools = preload("res://packed/items/Items.tscn")
+onready var check = preload("res://assets/img/progsource/checkmark.png")
 
 func start():
 	if requirements.size() == 0:
@@ -21,12 +29,23 @@ func start():
 		item.scale.x = 0.5
 		item.scale.y = 0.5
 		item.position = pos[i]
+	for i in range(checkmarks.size()):
+		checkmarks[i].visible = false
+		add_child(checkmarks[i])
+		var checkmark = TextureRect.new()
+		checkmark.texture = check
+		checkmarks[i].add_child(checkmark)
+		checkmarks[i].scale.x = 0.5
+		checkmarks[i].scale.y = 0.5
+		checkmarks[i].position.x = pos[i].x - 4
+		checkmarks[i].position.y = 1
 
 # warning-ignore:unused_argument
 func _process(delta):
 	if requirements.size() == 0:
 		return
 	for i in range(requirements.size()):
-		if requirements[i].requirement_satisfied:
+		if requirements[i].requirement_satisfied && get_child(i).visible:
 			# warning-ignore:return_value_discarded
-			get_child(i).modulate = Color(0, 1, 1)
+			get_child(i).visible = false
+			checkmarks[i].visible = true
